d85919090a04e620e62942206bad06586b6a3168
Shorif4321 <shorifali4321@gmail.com>
2022-09-18 11:23:01 +0600

    git cmmit

--- src/Component/Home/Service/SingleService/SingleService.jsx
+++ src/Component/Home/Service/SingleService/SingleService.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./SingleService.css"
 import { Link } from 'react-router-dom';
 
 const SingleService = (props) => {
@@ -11,7 +12,7 @@ const SingleService = (props) => {
                 <div className="card-body">
                     <h4 className="card-title text-justify service-h4"> {title} </h4>
                     <div />
-                    <p className="card-text text-justify service-p" dangerouslySetInnerHTML={{ __html: description?.slice(0, 50) }} />
+                    <p className="card-text text-justify service-p" dangerouslySetInnerHTML={{ __html: description?.slice(0, 45) }} />
                     <div className="custom-btn">
                         <Link to={`/serviceDetails/${id}`} className="btn btn-success btn-card bg-none float-left p-0">Read More <i
                             style={{ color: "#00A652", marginLeft: "5px" }} className="fa fa-arrow-right"></i></Link>
